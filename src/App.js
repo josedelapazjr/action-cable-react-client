3ff95161360c07d903855d6d70fc23a4f393c26e
Joe <jwataru@gmail.com>
2016-12-30 17:50:49 +1100

    Remove unnecessary packages.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import ActionCable from 'actioncable'
-import ActionCableProvider from 'react-actioncable-provider';
+import ActionCable from 'actioncable';
 const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
 import MyApp from './MyApp';
 
@@ -8,12 +7,9 @@ class App extends Component {
 
   render(){
     return (
-      <ActionCableProvider cable={cable}>
         <MyApp cable={cable}/>
-      </ActionCableProvider>
     );
   }
-
 }
 
 export default App;
